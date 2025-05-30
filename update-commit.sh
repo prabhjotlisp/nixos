c736f02295d039d55298918d8607d23fa3a60e0a
prabhjotlisp <prabhjotsingh.lisp@gmail.com>
2025-05-30 21:56:55 +0530

    Stable Fri May 30 09:56:55 PM IST 2025

--- update-commit.sh
+++ update-commit.sh
@@ -1,5 +1,4 @@
 Dir=/home/babu/.config/nixos
 git -C $Dir add -A &&
-git -C $Dir commit -m "$(date)" &&
-sudo nixos-rebuild switch --flake $Dir/#msi &&
+git -C $Dir commit -m "Stable $(date)" &&
 git -C $Dir push 
