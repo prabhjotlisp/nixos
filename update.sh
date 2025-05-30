2695204e95e057d102b89a056e390684d67b2c4e
prabhjotlisp <prabhjotsingh.lisp@gmail.com>
2025-05-30 21:55:02 +0530

    Fri May 30 09:55:02 PM IST 2025

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
-git -C /home/babu/.config/nixos/ add -A &&
-sudo nixos-rebuild switch --flake /home/babu/.config/nixos/#msi
+Dir=/home/babu/.config/nixos
+git -C $Dir add -A && git -C $Dir commit -m "$(date)" && sudo nixos-rebuild switch --flake $Dir/#msi
